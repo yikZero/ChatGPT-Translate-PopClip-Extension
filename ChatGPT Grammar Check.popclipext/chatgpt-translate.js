83d5338f4f2b6fbf4a83867a659db586beec04c0
yikZero <i@yikzero.com>
2023-03-09 13:59:33 +0800

    调整chatgpt描述与函数名称

--- ChatGPT Grammar Check.popclipext/chatgpt-translate.js
+++ ChatGPT Grammar Check.popclipext/chatgpt-translate.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.actions = void 0;
 const axios_1 = require("axios");
 
-const checkGrammar = async (input, options) => {
+const translateText = async (input, options) => {
   const openai = axios_1.default.create({
     baseURL: "https://api.openai.com/v1",
     headers: { Authorization: `Bearer ${options.apikey}` },
   });
-  const prefix = "请将以下内容翻译成中文，如果内容本身是中文则翻译成英文，仅需输出翻译后的结果:\n\n";
+  const prefix = "请将以下内容翻译成中文，如果内容本身是中文则翻译成英文，仅需输出翻译后的结果，如果翻译内容本身有符号，输出相对应的符号，如果没有符号禁止自行添加符号:\n\n";
   // send the whole message history to OpenAI
   const { data } = await openai.post("chat/completions", {
     model: "gpt-3.5-turbo",
@@ -26,5 +26,5 @@ const checkGrammar = async (input, options) => {
 // export the actions
 exports.actions = [{
   title: "ChatGPT: Translate",
-  code: checkGrammar,
+  code: translateText,
 }];
